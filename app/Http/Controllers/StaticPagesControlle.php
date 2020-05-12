412a4f2d2c9bd658411ec24e9d14e15cb1e1d7c3
Your Name <you@example.com>
2020-05-12 14:23:22 +0000

    Initial commit

---
+++ app/Http/Controllers/StaticPagesControlle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesControlle extends Controller
+{
+
+  public function home()
+  {
+
+  	return view('static_pages/home');
+
+  }
+
+  public function help()
+  {
+
+  	return view('static_pages/help');
+
+  }
+
+  public function about()
+  {
+
+  	return view('static_pages/about');
+
+  }
+}
