8eb6605b39860e81352b8f6e329e0ec822695fb9
Your Name <you@example.com>
2020-05-19 12:23:41 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -25,4 +25,12 @@ class StatusesController extends Controller
     	session()->flash('success', '发布时间');
     	return redirect()->back();
     }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status); //做删除授权的检测，不通过会抛出 403 异常。
+        $status->delete(); //调用 Eloquent 模型的 delete 方法对该微博进行删除。
+        session()->flash('success', '微博已被成功删除！'); //删除成功之后，将返回到执行删除微博操作的页面上。
+        return redirect()->back();
+    }
 }
