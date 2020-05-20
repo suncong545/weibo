6df64032ef3f2d6290a497102be8f4010f398c15
Your Name <you@example.com>
2020-05-20 07:29:34 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,9 +28,6 @@ class User extends Authenticatable
         将小写的邮箱使用 md5 方法进行转码；
         将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；*/
     }
-     
-
-
 
     protected $table = 'users';
 
@@ -74,5 +71,36 @@ class User extends Authenticatable
     {
         return $this->statuses()
                     ->orderBy('created_at','desc');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+     public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
